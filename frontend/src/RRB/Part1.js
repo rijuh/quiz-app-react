379a31f05a2d73b2a9e47029e3224c7d14ab9505
rijuh <ankit.hazra.739@gmail.com>
2026-02-24 00:43:51 +0530

    RRB Qus Test

---
+++ frontend/src/RRB/Part1.js
@@ -0,0 +1,32 @@
+export const GeographyMCQ = [
+    {
+        question: "The role of the Himalayas in shaping India's climate includes all, EXCEPT which of the following?",
+        options: [
+            "Contributing to snowfall in northern India",
+            "Acting as a barrier to southwest monsoon winds",
+            "Creating convection currents",
+            "Blocking cold winds from Central Asia"
+        ],
+        answer: "Creating convection currents"
+    },
+    {
+        question: "Which of the following cities is located in a rain-shadow region in India?",
+        options: ["Shillong", "Cochin", "Goa", "Pune"],
+        answer: "Pune"
+    },
+    {
+        question: "Which of the following animals is listed as endangered and protected under the Indian Wildlife Protection Act, 1972?",
+        options: ["Peacock", "Lion-tailed macaque", "Nilgai", "Hanuman langur"],
+        answer: "Lion-tailed macaque"
+    },
+    {
+        question: "Acid rain is mainly caused due to the presence of which two gases in the atmosphere?",
+        options: [
+            "Oxygen and Hydrogen",
+            "Sulphur dioxide and Nitrogen oxides",
+            "Carbon dioxide and Carbon monoxide",
+            "Nitrogen and Methane"
+        ],
+        answer: "Sulphur dioxide and Nitrogen oxides"
+    },
+]
